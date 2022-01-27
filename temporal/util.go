e5587da1371b54290694d5afdf3cdae7d3c77260
Caleb Hiebert <calebkhiebert@gmail.com>
2022-01-27 09:21:42 -0600

    feat: add temporal utilities

---
+++ temporal/util.go
@@ -0,0 +1,91 @@
+package fct_temporal
+
+import (
+	"go.temporal.io/sdk/temporal"
+	"os"
+	"testing"
+	"time"
+
+	"go.temporal.io/sdk/workflow"
+)
+
+const (
+	ActivityTimeoutS  = 25 * time.Second
+	ActivityTimeoutM  = 1 * time.Minute
+	ActivityTimeoutL  = 10 * time.Minute
+	ActivityTimeoutXL = 25 * time.Minute
+)
+
+var (
+	ActivityMaxRetriesNone = &temporal.RetryPolicy{
+		InitialInterval:    time.Second,
+		BackoffCoefficient: 2,
+		MaximumInterval:    time.Minute,
+		MaximumAttempts:    1,
+	}
+
+	ActivityMaxRetriesS = &temporal.RetryPolicy{
+		InitialInterval:    time.Second,
+		BackoffCoefficient: 2,
+		MaximumInterval:    ActivityTimeoutM,
+		MaximumAttempts:    3,
+	}
+
+	ActivityMaxRetriesM = &temporal.RetryPolicy{
+		InitialInterval:    time.Second,
+		BackoffCoefficient: 2,
+		MaximumInterval:    ActivityTimeoutXL,
+		MaximumAttempts:    15,
+	}
+
+	ActivityMaxRetriesL = &temporal.RetryPolicy{
+		InitialInterval:    time.Second,
+		BackoffCoefficient: 2,
+		MaximumInterval:    ActivityTimeoutXL,
+		MaximumAttempts:    75,
+	}
+
+	ActivityMaxRetriesUnlimited = &temporal.RetryPolicy{
+		InitialInterval:    time.Second,
+		BackoffCoefficient: 2,
+		MaximumInterval:    ActivityTimeoutXL,
+	}
+)
+
+const StandardHeartbeatSpacing = 10 * time.Second
+
+// activityCtx is a helper to create a context with workflow.ActivityOptions attached
+// The activity options will be created with a max run duration of timeout
+func activityCtx(ctx workflow.Context, timeout time.Duration, retry *temporal.RetryPolicy) workflow.Context {
+	return workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		StartToCloseTimeout: timeout,
+		RetryPolicy:         retry,
+	})
+}
+
+// activityCtxDC is a helper to create a disconnected context with workflow.ActivityOptions attached
+// The activity options will be created with a max run duration of timeout
+func activityCtxDC(ctx workflow.Context, timeout time.Duration) workflow.Context {
+	dCtx, _ := workflow.NewDisconnectedContext(ctx)
+
+	return workflow.WithActivityOptions(dCtx, workflow.ActivityOptions{
+		StartToCloseTimeout: timeout,
+	})
+}
+
+// activityCtxHB is a helper to create a context with workflow.ActivityOptions attached
+// The activity options will be created with a max run duration of timeout
+// And a default heartbeat spacing of StandardHeartbeatSpacing
+func activityCtxHB(ctx workflow.Context, timeout time.Duration, retry *temporal.RetryPolicy) workflow.Context {
+	return workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		StartToCloseTimeout: timeout,
+		HeartbeatTimeout:    StandardHeartbeatSpacing,
+		RetryPolicy:         retry,
+	})
+}
+
+func skipCI(t *testing.T) {
+	if os.Getenv("CI") != "" {
+		t.Skip("Skipping testing in CI environment")
+	}
+}
