cffa70be518c4c100afd173fcd52203e9adee8ef
Caleb Hiebert <calebkhiebert@gmail.com>
2022-06-01 14:43:13 -0500

    fix: merge main

--- helpers.go
+++ helpers.go
@@ -3,8 +3,11 @@ package fcthelp
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"github.com/datomar-labs-inc/FCT_Helpers_Go/ferr"
 	"reflect"
+	"time"
+	"unsafe"
 )
 
 func StrPtr(s string) *string {
@@ -116,4 +119,30 @@ type Array[T any] []T
 
 func (a *Array[T]) Push(item T) {
 	*a = append(*a, item)
+}
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func RandString(n int) string {
+	b := make([]byte, n)
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return *(*string)(unsafe.Pointer(&b))
 }
